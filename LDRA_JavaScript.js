8c001e0c65c84c4f7906bbfd16f90bfd6cfecab9
Brimercam <brimercam@gmail.com>
2018-11-12 20:46:32 -0500

    Secon commit

---
+++ LDRA_JavaScript.js
@@ -0,0 +1,18 @@
+/* const welcome = ("Hello and welcome the the LDRA Homepage!");
+window.alert (welcome); */
+
+window.onscroll = () => {
+    const nav = document.querySelector('#navbar');
+    if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
+  };
+
+function initMap() {
+    // The location of Tom Sawyer Park
+    let cherokee = {lat: 38.244566, lng: -85.694697};
+    let oldLou = {lat: 38.233092, lng: -85.753058};
+    // The map, centered at Towm Sawyer Park
+    let map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 11, center: oldLou, cherokee});
+    // The marker, positioned at 
+    let marker = new google.maps.Marker({position: oldLou, cherokee, map: map});
+  }
