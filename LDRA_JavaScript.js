e09d56c6c3a319e1c0ff5ed0891d0cc99c585c6b
Brimercam <brimercam@gmail.com>
2018-11-16 10:09:10 -0500

    Function adjustments

--- LDRA_JavaScript.js
+++ LDRA_JavaScript.js
@@ -1,22 +1,76 @@
  /*const welcome = ("Hello and welcome the the LDRA Homepage!");
-window.alert (welcome);
+window.alert (welcome);*/
 
-/*Currently working on fixing this so #navbar will change color with scrolling
 
-window.onscroll = () => {
-    const nav = document.querySelector('#navbar');
-    if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
-};
+//Goolge map API function each variable creates locations and repective info
+  function dogRunMap() {
 
-*/
+	let tomsawyer = {
+		info: '<h1>Tom Sawyer Dog Run</strong></h1>\
+                   3000 Freys Hill Rd<br> Louisville, Ky 40241<br>\
+					<a href="https://goo.gl/maps/adjeQmSTQxk">Get Directions</a>',
+		lat: 38.2828,long: -85.5586
+	};
 
-function initMap() {
-    // The location of Tom Sawyer Park
-    let cherokee = {lat: 38.244566, lng: -85.694697};
-    let oldLou = {lat: 38.233092, lng: -85.753058};
-    // The map, centered at Towm Sawyer Park, Dog Run
-    let map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 11, center: oldLou, cherokee});
-    // The marker, positioned at Tom Sawyer Dog Run
-    let marker = new google.maps.Marker({position: oldLou, cherokee, map: map});
-  }
+	let oldLou = {
+		info: '<h1>Old Louisville Dog Run</strong></h1>\
+				     <br> Louisville, Ky 40203<br>\
+					<a href="https://goo.gl/maps/EvYgWM3QQ2R2">Get Directions</a>',
+		lat: 38.2330,long: -85.7530
+	};
+
+	let champions = {
+		info: '<h1>Champions Park Dog Run</strong></h1>\r\
+                    2050 River Rd,<br> Louisville, ky 40206<br>\
+					<a href="https://goo.gl/maps/7DdvQFQ8qcG2">Get Directions</a>',
+		lat: 38.2681,long: -85.7135 
+    };
+    
+    let cochran = {
+		info: '<h1>Cochran Hill Dog Run</strong></h1>\r\
+                  740 Cochran Hill Rd,<br> Louisville, ky 40206<br>\
+					<a href="https://goo.gl/maps/7DdvQFQ8qcG2">Get Directions</a>',
+		lat: 38.2442,long: -85.6953  
+    };
+           
+    let floyds = {
+		info: '<h1>Beckley Creek Pkwy,</h1><br>\r\
+                      1411 Beckley Creek Pkwy,,<br> Louisville, ky 40245<br>\
+					<a href="https://goo.gl/maps/3ayy6rp3Yh52">Get Directions</a>',
+		lat: 38.2082,long: -85.4749   
+	};
+
+
+      //Array to connect each park variables info., lat., and long. into "let parks" 
+	let parks = [
+      [tomsawyer.info, tomsawyer.lat, tomsawyer.long, 0],
+      [oldLou.info, oldLou.lat, oldLou.long, 1],
+      [champions.info, champions.lat, champions.long, 2],
+      [cochran.info, cochran.lat, cochran.long, 3],
+      [floyds.info, floyds.lat, floyds.long, 4],
+    ];
+
+	let map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 11,
+		center: new google.maps.LatLng(38.2527, -85.7585),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
+
+	let infowindow = new google.maps.InfoWindow({});
+
+	let marker, i;
+
+	for (i = 0; i < parks.length; i++) {
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(parks[i][1], parks[i][2]),
+			map: map
+        });
+        
+		google.maps.event.addListener(marker, 'click', (function (marker, i) {
+			return function () {
+				infowindow.setContent(parks[i][0]);
+				infowindow.open(map, marker);
+			}
+		})(marker, i));
+	}
+}
--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # LouisvilleDogRunLDRA
-Overhaul of the existing Louisville Dog Run website, which is need of a serious update (http://www.louisvilledogs.org/).
+Overhaul of the existing Louisville Dog Run website, which is need of a serious update (http://www.louisvilledogs.org/). This project
+presented many challenges for me and still does, especially balance between design and function. I wanted to build from the ground up w/ no help from things like Bootstrap. Although it's not complete I'm going to finish this site w/ every intention of presenting to the park as a complete replacement. 
 
-#Custom CSS
+# CSS
 All of the CSS is custom, however these stand out:
 1. body, p, and h1- needed to be set w/ a margin: opx;, b/c they created a large margin of space at the 
 top of the page which pushed the fixed navigation bar down too much. 
 2. #navbar- I created to be fixed w/ a translucent black background, that includes a hover action that changes the link colors.  
 3. @media queries- are set to function for cellphones and tablets. Each query resizes all images and text including the navigation bar. 
 
-#Custom JavaScript 
-1. initMap- is a custom google map that uses the location of the main headquarters of LDRA.
+# JavaScript 
+1. dogRunMap- is a custom Google API that creates markers for all locations on one map. The user can click each marker and gain information along with directions. 
 
